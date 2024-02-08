f70edfc1b66ee8f9819fe1318147b2f6cee5c341
Diego Cairone <diegocairone@gmail.com>
2024-02-08 12:11:38 -0300

    initialization

--- src/test/java/com/cairone/AppTest.java
+++ src/test/java/com/cairone/AppTest.java
@@ -1,5 +1,12 @@
 package com.cairone;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class AppTest {
 
+    @Test
+    public void foo() {
+        Assertions.assertThat(1).isEqualTo(1);
+    }
 }
