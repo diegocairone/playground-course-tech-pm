0524f7f02cf253105899b7a79d63b92289646d06
Diego Cairone <diegocairone@gmail.com>
2024-02-08 18:53:43 -0300

    wip: assay - adding netty/jersey

---
+++ src/main/java/com/cairone/rest/AritmeticCtrl.java
@@ -0,0 +1,31 @@
+package com.cairone.rest;
+
+import com.cairone.calc.AritmeticCalc;
+import com.cairone.rest.resource.ResultResource;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+@Path("foo")
+public class AritmeticCtrl {
+
+    private AritmeticCalc aritmeticCalc;
+
+    public AritmeticCtrl() {
+        this.aritmeticCalc = new AritmeticCalc();
+    }
+
+    @GET
+    @Path("ok")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response foo() {
+
+        int result = aritmeticCalc.sumOf(3, 4);
+        ResultResource response = ResultResource.builder()
+                .result(result)
+                .build();
+        return Response.ok(response).build();
+    }
+}
---
+++ src/main/java/com/cairone/rest/resource/ResultResource.java
@@ -0,0 +1,11 @@
+package com.cairone.rest.resource;
+
+import lombok.Builder;
+import lombok.RequiredArgsConstructor;
+
+@Builder
+@RequiredArgsConstructor
+public class ResultResource {
+
+    private final Integer result;
+}
